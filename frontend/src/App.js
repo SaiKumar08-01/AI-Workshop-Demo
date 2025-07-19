3be2dfd7ff9fac3f251049372dd7d7d54cda5cd2
Sai Kumar <saikumargadhapu@gmail.com>
2025-07-19 16:15:51 +0530

    Initial commit on AI-Workshop-Demo branch

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ChatBox from "./component/ChatBox";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>🎓 AI Study Buddy</h1> */}
+      <ChatBox />
+    </div>
+  );
+}
+export default App;
